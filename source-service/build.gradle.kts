de1d06149717676a23d07d78ec05aea57807f2a7
Moisés Rocha <moisesrocha99@gmail.com>
2025-06-11 01:57:28 +0100

    Create source service

---
+++ source-service/build.gradle.kts
@@ -0,0 +1,45 @@
+plugins {
+    id("mihon.android.application")
+}
+
+android {
+    namespace = "com.example.source_service"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.example.source_service"
+        minSdk = 35
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+}
+
+dependencies {
+
+    implementation(androidx.corektx)
+    implementation(androidx.appcompat)
+    implementation(libs.material)
+//    testImplementation(libs.junit)
+//    androidTestImplementation(libs.ext.junit)
+//    androidTestImplementation(libs.espresso.core)
+}
